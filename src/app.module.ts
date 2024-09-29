1f72aae7843e260d6486305f18596648b0943523
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-09-29 23:10:43 +0100

    :bug: chores:fixes in the app.module section

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, AuthModule,DatabaseModule],
+  imports: [TasksModule, AuthModule, DatabaseModule,
+    ConfigModule.forRoot({
+    isGlobal: true, 
+  }),],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], 
 })
-export class AppModule {}
+export class AppModule {}
